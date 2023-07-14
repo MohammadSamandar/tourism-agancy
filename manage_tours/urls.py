5e858630a2ec5c17391ad2fd6dd6402e859637eb
mohammad samandar <m.samandarhd@gmail.com>
2023-07-14 12:40:59 +0330

    update tour-search-result url

--- manage_tours/urls.py
+++ manage_tours/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('tour-list/', views.tour_list, name='tour-list'),
     path('<slug:slug>', views.tour_detial, name='tour-detail'),
     path('tour-booking/', views.tour_booking, name='tour-booking'), # صفحه پرداخت
-    path('tour-search-result/', views.tour_search_result, name='tour-search-result'),
+    path('search-result/', views.tour_search_result, name='tour-search-result'),
 
 
 ]
